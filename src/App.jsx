5f21b460f3374bd30264f89403cb2d4c90c27a34
John Dungao <johnmiguel.dungao@gmail.com>
2023-11-14 18:45:05 -0500

    fix minor bugs

--- src/App.jsx
+++ src/App.jsx
@@ -12,9 +12,9 @@ import SignIn from "./components/SignIn/SignIn";
 function App() {
   //States
   const [imgURL, setImgURL] = useState("");
-  const [image, setImage] = useState("");
   const [box, setBox] = useState({});
   const [route, setRoute] = useState("signin");
+  const [isSignedIn, setIsSignedIn] = useState(false);
 
   const calcFaceLoc = (data) => {
     const faceBox = data.outputs[0].data.regions[0].region_info.bounding_box;
@@ -37,29 +37,43 @@ function App() {
     setImgURL(evt.target.value);
   };
 
+  useEffect(() => {}, [setImgURL]);
+
   const onBtnSubmit = async () => {
-    setImage(imgURL);
     let data = await sendImagePrediction(imgURL);
     let faceCalcData = await calcFaceLoc(data);
+    console.log(faceCalcData);
     await displayFaceBox(faceCalcData);
+  };
+
+  const onRouteChange = (route) => {
+    if (route === "signin") {
+      setRoute("signin");
+      setIsSignedIn(false);
+    } else if (route === "home") {
+      setRoute("home");
+      setIsSignedIn(true);
+    } else {
+      setRoute("register");
+    }
   };
 
   return (
     <div className="App">
       <ParticlesBg type="cobweb" bg={true} />
-      <Navigation />
-      {route === "signin" ? (
-        <SignIn />
-      ) : (
-        <>
+      <Navigation isSignedIn={isSignedIn} onRouteChange={onRouteChange} />
+      {route === "home" ? (
+        <div>
           <Logo />
           <Rank />
           <ImageLinkForm
             onInputChange={onInputChange}
             onBtnSubmit={onBtnSubmit}
           />
-          <FaceRecognition box={box} imageURL={image} />
-        </>
+          <FaceRecognition box={box} imageURL={imgURL} />
+        </div>
+      ) : (
+        <SignIn onRouteChange={onRouteChange} />
       )}
     </div>
   );
